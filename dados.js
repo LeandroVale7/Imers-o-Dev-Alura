658aa96d00bea25c6e7f75c6eb5a9393acde34d6
Leandro do Vale <118278969+LeandroVale7@users.noreply.github.com>
2024-09-06 17:02:15 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -3,18 +3,40 @@ let dados = [
         titulo : "Lionel Messi",
         descricao : "O maior vencedor da Bola de Ouro é Lionel Messi. O craque argentino conquistou esse prestigiado prêmio por oito vezes ao longo de sua carreira, consolidando seu status como um dos maiores jogadores de futebol de todos os tempos.",
         link : "https://pt.wikipedia.org/wiki/Lionel_Messi",
-        tags : "m10 et"
+        tags : "m10  et  argentina barcelona psg"
     }, 
     {
         titulo: "Cristiano Ronaldo",
         descricao: "O segundo maior vencedor da Bola de Ouro é Cristiano Ronaldo. O astro português conquistou esse prêmio cinco vezes ao longo de sua carreira, um feito impressionante.",
         link: "https://pt.wikipedia.org/wiki/Cristiano_Ronaldo",
-        tags : "cr7 goat robozao"
+        tags : "cr7 goat robozao portugal manchester realmadrid juventus"
     },
     
     {
         titulo: "Johan Cruyff",
         descricao: "Johan Cruyff é um dos maiores vencedores da Bola de Ouro, com três conquistas. O holandês, considerado um dos maiores jogadores de todos os tempos, revolucionou o futebol com seu estilo de jogo elegante e criativo.",
         link: "https://pt.wikipedia.org/wiki/Johan_Cruyff",
-        tags : "holanda revolucionario"
-    }];
+        tags : "holanda ajax"
+    },
+
+    {
+        titulo: "Kaká",
+        descricao: "O brasileiro Kaká conquistou a Bola de Ouro em 2007, após uma temporada brilhante com o Milan e a seleção brasileira.",
+        link: "https://pt.wikipedia.org/wiki/Kaká",
+        tags: "brasil, milan, 2007"
+    },
+
+    {
+        titulo: "Luka Modrić",
+        descricao: "O croata Luka Modrić surpreendeu a todos ao conquistar a Bola de Ouro em 2018, após liderar a Croácia à final da Copa do Mundo.",
+        link: "https://pt.wikipedia.org/wiki/Luka_Modrić",
+        tags: "croácia, real madrid, 2018"
+    },
+
+    {
+        titulo: "Roberto Baggio",
+        descricao: "O italiano Roberto Baggio conquistou a Bola de Ouro em 1993, sendo um dos maiores jogadores da história do futebol italiano.",
+        link: "https://pt.wikipedia.org/wiki/Roberto_Baggio",
+        tags: "italia, juventus, 1993"
+    }
+];
